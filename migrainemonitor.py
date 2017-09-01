7a463de691dda06207d887af7bc1ba743eb1f7f9
Barry Bond <barrybo@microsoft.com>
2017-08-31 19:31:25 -0700

    Graph pressure to the OLED screen Misc. other fixes

--- migrainemonitor.py
+++ migrainemonitor.py
@@ -29,27 +29,86 @@ import thread
 import Adafruit_BMP.BMP085 as BMP085
 import os
 import RPi.GPIO as GPIO
+import smbus
+
+import spidev as SPI
+import SSD1306
+import Image
+import ImageDraw
+import ImageFont
+import collections
+
+address = 0x20 # Buzzer and LED2 is controlled by PCF8574 at i2c 0x20
+LED = 26 # GPIO pin, 21 on my breadboard 
+KEY = 20 # GPIO pin for joystick.  Button was 23 on my breadboard
+
+pressurelog = collections.deque(maxlen=128)
+
+bus = smbus.SMBus(1)
+
+def beep_on():
+	bus.write_byte(address,0x7F&bus.read_byte(address))
+def beep_off():
+	bus.write_byte(address,0x80|bus.read_byte(address))
 
 def ButtonThread(a, *args):
   fo = open('migraine_start_log.txt', 'a')
   while True:
-    GPIO.wait_for_edge(23, GPIO.FALLING)
+    GPIO.wait_for_edge(KEY, GPIO.FALLING) # can read from 'address' to check which direction the joystick has been moved
+    beep_on()
     dt = datetime.now().isoformat()
-    fo.write('{0}\n'.format(dt))
+    fo.write('{0},localtime\n'.format(dt))
     fo.flush()
     time.sleep(0.25) # debouncing delay
+    beep_off()
 
 def SetLED(v):
-  GPIO.output(21, v)
+  GPIO.output(LED, v)
+
+def DrawCentered(draw, font, y, m):
+   (w,h) = draw.textsize(m, font)
+   x = (128-w)/2
+   draw.text((x,y), m, font=font, fill=255)
+
+spi = SPI.SpiDev(0,0)
+disp = SSD1306.SSD1306(19,16,spi)
+disp.begin()
+disp.clear()
+disp.display()
+image = Image.new('1', (disp.width, disp.height))
+draw = ImageDraw.Draw(image)
+fontheader = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeSans.ttf',14)
+font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeSans.ttf',20)
+disp.set_contrast(0)
+
+# Light blue region is 128x48
+# Yellow region is 128x16
+def UpdateDisplay():
+   DrawCentered(draw, fontheader, 48, 'Migraine Monitor')
+   maxp = max(pressurelog)
+   x=0
+   for p in pressurelog:
+    y = (p*48)/maxp
+    draw.line([(x,0),(x,48)]) # erase the old pixel in this column
+    draw.point((x,(48-y)), fill=255) # draw the new one
+    x = x+1   
+   disp.image(image)
+   disp.display()
+   
+def UIThread(a, *args):
+ while True:
+   time.sleep(4.8)
+   SetLED(GPIO.HIGH)
+   time.sleep(0.2)
+   SetLED(GPIO.LOW)
 
 time.sleep(5) # give the OS plenty of time to boot
 
 GPIO.setmode(GPIO.BCM)
-# set GPIO21 to be the LED
-GPIO.setup(21, GPIO.OUT)
-# set GPIO23 to be the button
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(LED, GPIO.OUT)
+GPIO.setup(KEY, GPIO.IN, GPIO.PUD_UP)
 thread.start_new_thread(ButtonThread, ('', ''))
+thread.start_new_thread(UIThread, ('', ''))
 
 # Default constructor will pick a default I2C bus.
 #
@@ -59,7 +118,7 @@ thread.start_new_thread(ButtonThread, ('', ''))
 #
 # For the Beaglebone Black the library will assume bus 1 by default, which is
 # exposed with SCL = P9_19 and SDA = P9_20.
-sensor = BMP085.BMP085()
+#sensor = BMP085.BMP085()
 
 # You can also optionally change the BMP085 mode to one of BMP085_ULTRALOWPOWER,
 # BMP085_STANDARD, BMP085_HIGHRES, or BMP085_ULTRAHIGHRES.  See the BMP085
@@ -74,10 +133,9 @@ while True:
   pressure = sensor.read_pressure()
   altitude = sensor.read_altitude()
   sealevel_pressure = sensor.read_sealevel_pressure()  
-  fo.write('{0},{1:0.2f},{2:0.2f},{3:0.2f},{4:0.2f}\n'.format(dt, temperature, pressure, altitude, sealevel_pressure))
+  fo.write('{0},{1:0.2f},{2:0.2f},{3:0.2f},{4:0.2f},localtime\n'.format(dt, temperature, pressure, altitude, sealevel_pressure))
   fo.flush()
-  time.sleep(4.8)
-  SetLED(True)
-  time.sleep(0.2)
-  SetLED(False)
+  pressurelog.append(pressure)
+  UpdateDisplay()
+  time.sleep(60)
   
